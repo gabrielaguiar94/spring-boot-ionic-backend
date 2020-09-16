ce632359f41d3d6f1f9d356b441347e87bd65deb
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-09-15 22:00:47 -0300

    Banco de dados H2 e criacao automatica da base de dados a partir dos objetos

--- src/main/java/com/gabrielaguiar/cursomc/domain/Categoria.java
+++ src/main/java/com/gabrielaguiar/cursomc/domain/Categoria.java
@@ -2,10 +2,17 @@ package com.gabrielaguiar.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {	
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //criar a coluna ID no banco de dados como PK (PRIMARY KEY)
 	private Integer id;
 	private String nome;
 	
